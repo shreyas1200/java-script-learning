963a79473a822f8e8296d2993e640502340fdf56
shreyas <creativespace122@gmail.com>
2026-05-01 10:52:17 +0530

    index html modified , condirtional logic learning , checkout flow updated

--- js learning folder/conditonal-logic.js
+++ js learning folder/conditonal-logic.js
@@ -2,21 +2,55 @@
 
 //letts store user name // 
 
-const username ="Shreyas"
+// const username ="Shreyas"
 
-if (username === "admin"){
+// if (username === "admin"){
+//     console.log("welcome admin")
+// } else {console.log ( "acess denied ")}
+
+
+// // writing cart logic//
+
+// let cartTotal = 600
+
+// if (cartTotal >= 1000){
+//     console.log("free delivery")
+// } else if (cartTotal >= 500 && cartTotal < 1000){
+//     console.log("delivery charge ₹50")
+// } else {
+//     console.log("delivery charge ₹100")
+// }
+
+// practice problems //
+
+// create a variable to store the user name as adimin 
+
+const userName ="admin"
+
+if(userName === "admin") {
     console.log("welcome admin")
-} else {console.log ( "acess denied ")}
-
-
-// writing cart logic//
-
-let cartTotal = 600
-
-if (cartTotal >= 1000){
-    console.log("free delivery")
-} else if (cartTotal >= 500 && cartTotal < 1000){
-    console.log("delivery charge ₹50")
 } else {
-    console.log("delivery charge ₹100")
+    console.log("access denied")
 }
+
+
+//  problem 2 shop delivery charges logic //
+
+let cartTotal = 700
+
+if (cartTotal >=1000 ) {
+    console.log("free delivery")
+} else if ( cartTotal>=500) {console.log("delivery charge ₹50")}
+
+else {console.log("delivery charge ₹100")}
+
+
+// problem 3 check age for ticket price //
+
+const userAge = 60
+
+if (userAge >= 60) {console.log("ticket price ₹100")}
+
+else if (userAge >= 18) {console.log("ticket price ₹200")}
+
+else {console.log("ticket price ₹50")}
